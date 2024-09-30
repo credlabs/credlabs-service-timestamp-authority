3f04133de062703df661f35403776ecb63eba44f
James Kent <jkent@blueengine.com.au>
2024-09-30 17:23:13 +1000

    Basic

--- cmd/timestamp-lambda/main.go
+++ cmd/timestamp-lambda/main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"context"
-	"encoding/json"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
@@ -13,17 +11,8 @@ func main() {
 	lambda.Start(HandleRequest)
 }
 
-func HandleRequest(ctx context.Context, event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+func HandleRequest(ctx context.Context) (events.APIGatewayProxyResponse, error) {
 	log.Println("Hello World!")
-
-	json_msg, err := json.Marshal(event)
-
-	if err == nil {
-			panic(err)
-	}//if
-
-	str := string(json_msg)
-	fmt.Println(str)
 
 	response := events.APIGatewayProxyResponse{
 			StatusCode: 200,
