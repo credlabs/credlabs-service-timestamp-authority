e7a27e8941e11fe470e5d617ca5349806eeb88f2
James Kent <jkent@blueengine.com.au>
2024-09-30 23:37:06 +1000

    Demo

--- cmd/timestamp-lambda/main.go
+++ cmd/timestamp-lambda/main.go
@@ -1,42 +1,23 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"log"
-	"encoding/json"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-type CredlabsTimestampRequest struct {
-	Name *string `json:"name"`
+type MyEvent struct {
+	Name string `json:"name"`
 }
 
-type CredlabsTimestampResponse struct {
-	Status *string `json:"status"`
-}
-
-func HandleRequest(request *CredlabsTimestampRequest) (*CredlabsTimestampResponse, error) {
-	log.Println("Hello World!")
-
-	json_msg, err := json.Marshal(request)
-
-	if err == nil {
-			panic(err)
-	}//if
-
-	str := string(json_msg)
-	fmt.Println(str)
-
-	status := "success"
-
-	response := CredlabsTimestampResponse{
-			Status: &status,
+func HandleRequest(ctx context.Context, event *MyEvent) (*string, error) {
+	if event == nil {
+		return nil, fmt.Errorf("received nil event")
 	}
-
-	return &response, nil
+	message := fmt.Sprintf("Hello %s!", event.Name)
+	return &message, nil
 }
 
 func main() {
-	log.Println("Version: 1")
 	lambda.Start(HandleRequest)
 }
