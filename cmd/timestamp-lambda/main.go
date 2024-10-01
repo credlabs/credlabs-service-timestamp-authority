1d5409995b5b92d6ba975b33f3aab3fb73e6f296
James Kent <jkent@blueengine.com.au>
2024-10-01 15:04:32 +1000

    Update

--- cmd/timestamp-lambda/main.go
+++ cmd/timestamp-lambda/main.go
@@ -7,18 +7,25 @@ import (
 )
 
 type CredlabsTimestampRequest struct {
-	Name string `json:"name"`
+	PublicKeyUri string `json:"publicKeyUri"`
+	PrivateKeyUri string `json:"privateKeyUri"`
+	CertificateChainUri string `json:"certificateChainUri"`
+	Message string `json:"message"`
 }
 
 type CredlabsTimestampResponse struct {
 	Status string `json:"status"`
+	Result string `json:"result"`
 }
 
 func HandleRequest(ctx context.Context, event *CredlabsTimestampRequest) (*CredlabsTimestampResponse, error) {
-	if event == nil {
-		return nil, fmt.Errorf("received nil event")
-	}
-	message := fmt.Sprintf("Hello %s!", event.Name)
+	if event == nil { return nil, fmt.Errorf("Missing Credlabs Timestamp Request") }
+	if event.PublicKeyUri == "" { return nil, fmt.Errorf("Missing Public Key Reference") }
+	if event.PrivateKeyUri == "" { return nil, fmt.Errorf("Missing Private Key Reference") }
+	if event.CertificateChainUri == "" { return nil, fmt.Errorf("Missing Certificate Chain Reference") }
+	if event.Message == "" { return nil, fmt.Errorf("Missing Timestamp Message") }
+
+	message := fmt.Sprintf("Hello %s!", event.PublicKeyUri)
 
 	response := CredlabsTimestampResponse{
 		Status: message,
